2c14e9329d1b629050158296bf9a00d059b5bff8
Adrian Rodriguez <Adrian@Adrians-MacBook-Pro.local>
2014-08-04 23:57:26 -0500

    Finished login (including error handling, and caching). Added "keyboard disappear on background tap" functionality.

--- QRist/LoginViewController.m
+++ QRist/LoginViewController.m
@@ -12,35 +12,81 @@
 @interface LoginViewController ()
 
 @property (retain, nonatomic) IBOutlet UITextField *usernameInput;
-
+@property (retain, nonatomic) IBOutlet UITextField *passwordInput;
 
 @end
 
 @implementation LoginViewController
 
-- (IBAction)touchedLoginButton:(UIButton *)sender {
-    PFUser *user = [PFUser user];
-    user.username = self.usernameInput.text;
-    user.password = @"my pass";
+- (IBAction)touchedLoginButton:(UIButton *)sender
+{
+    if ([self loginInfoInputted]) {
+        [PFUser logInWithUsernameInBackground:self.usernameInput.text
+                                     password:self.passwordInput.text
+            block:^(PFUser *user, NSError *error) {
+                if (error) {
+                    UIAlertView *invalidLoginAlert = [[UIAlertView alloc] initWithTitle:@"Invalid Login"
+                                                                                message:@"Please try again."
+                                                                               delegate:self
+                                                                      cancelButtonTitle:@"OK"
+                                                                      otherButtonTitles:nil, nil];
+                
+                [invalidLoginAlert show];
+            } else [self performSegueWithIdentifier:@"login" sender:self];
+        }];
+    }
+}
+
+- (BOOL)loginInfoInputted
+{
+    if (!self.usernameInput.text || self.usernameInput.text.length == 0) {
+        UIAlertView *missingUsername = [[UIAlertView alloc] initWithTitle:@"Missing Username"
+                                                                  message:@"Please enter a username."
+                                                                 delegate:self
+                                                        cancelButtonTitle:@"OK"
+                                                        otherButtonTitles:nil, nil];
+        [missingUsername show];
+        return NO;
+    }
     
-    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (!error) {
-            NSLog(@"Blammo!");
-            [self performSegueWithIdentifier:@"login" sender:self];
-        } else {
-            NSLog(@"Error");
-        }
-    }];
+    if (!self.passwordInput.text || self.passwordInput.text.length == 0) {
+        UIAlertView *missingPassword = [[UIAlertView alloc] initWithTitle:@"Missing Password"
+                                                                  message:@"Please enter a password."
+                                                                 delegate:self
+                                                        cancelButtonTitle:@"OK"
+                                                        otherButtonTitles:nil, nil];
+        [missingPassword show];
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (void)dismissKeyboard:(id)sender
+{
+    [self.view endEditing:YES];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UITapGestureRecognizer *tapBackground = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard:)];
+    [tapBackground setNumberOfTapsRequired:1];
+    [self.view addGestureRecognizer:tapBackground];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    PFUser *currentUser = [PFUser currentUser];
+    
+    if (currentUser) {
+        [self performSegueWithIdentifier:@"login" sender:self];
+    }
 }
 
 /*
@@ -55,6 +101,7 @@
 
 - (void)dealloc {
     [_usernameInput release];
+    [_passwordInput release];
     [super dealloc];
 }
 @end
